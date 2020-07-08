4467bcdc65dce44832356f8ad5b344bbec119092
Cryptoforge <cryptoforge.cc@gmail.com>
2020-07-07 21:23:19 -0700

    update to libsodium 1.0.18

--- res/libsodium/buildlibsodium.sh
+++ res/libsodium/buildlibsodium.sh
@@ -2,31 +2,32 @@
 
 # First thing to do is see if libsodium.a exists in the res folder. If it does, then there's nothing to do
 if [ -f res/libsodium.a ]; then
-    exit 0
+    rm res/libsodium.a
+    rm -r res/libsodium/libsodium-1.0.18
 fi
 
 echo "Building libsodium"
 
 # Go into the lib sodium directory
 cd res/libsodium
-if [ ! -f libsodium-1.0.16.tar.gz ]; then
-    curl -LO https://download.libsodium.org/libsodium/releases/libsodium-1.0.16.tar.gz
+if [ ! -f libsodium-1.0.18.tar.gz ]; then
+    wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
 fi
 
-if [ ! -d libsodium-1.0.16 ]; then
-    tar xf libsodium-1.0.16.tar.gz
+if [ ! -d libsodium-1.0.18 ]; then
+    tar xf libsodium-1.0.18.tar.gz
 fi
 
 # Now build it
-cd libsodium-1.0.16
-LIBS="" ./configure
-make clean
+cd libsodium-1.0.18
+LIBS="" ./configure > /dev/null
+make clean > /dev/null 2>&1
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    make CFLAGS="-mmacosx-version-min=10.11" CPPFLAGS="-mmacosx-version-min=10.11" -j4
+    make CFLAGS="-mmacosx-version-min=10.11" CPPFLAGS="-mmacosx-version-min=10.11" > /dev/null 2>&1
 else
-    make -j8
+    make > /dev/null 2>&1
 fi
 cd ..
 
 # copy the library to the parents's res/ folder
-cp libsodium-1.0.16/src/libsodium/.libs/libsodium.a ../
+cp libsodium-1.0.18/src/libsodium/.libs/libsodium.a ../
