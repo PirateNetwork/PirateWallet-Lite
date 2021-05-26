d0b9e77942df2989d096f9aa771ce48e1dc6e64f
cryptoforge <cryptoforge.cc@gmail.com>
2021-05-26 08:53:21 -0700

    remove unneeded code

--- src/controller.cpp
+++ src/controller.cpp
@@ -262,7 +262,6 @@ void Controller::refreshAddresses() {
         return noConnection();
 
     auto newzaddresses = new QList<QString>();
-    //auto newtaddresses = new QList<QString>();
 
     zrpc->fetchAddresses([=] (json reply) {
         auto zaddrs = reply["z_addresses"].get<json::array_t>();
@@ -272,18 +271,6 @@ void Controller::refreshAddresses() {
         }
 
         model->replaceZaddresses(newzaddresses);
-
-        // auto taddrs = reply["t_addresses"].get<json::array_t>();
-        // for (auto& it : taddrs) {
-        //     auto addr = QString::fromStdString(it.get<json::string_t>());
-        //     if (Settings::isTAddress(addr))
-        //         newtaddresses->push_back(addr);
-        // }
-        //
-        // model->replaceTaddresses(newtaddresses);
-
-        // Refresh the sent and received txs from all these z-addresses
-        refreshTransactions();
     });
 
 }
