a6ae1219746dd55443288844ec711bdb7c5d7b84
Cryptoforge <cryptoforge.cc@gmail.com>
2022-12-11 23:49:02 -0800

    version 1.0.7

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,7 +1,7 @@
 export QT_STATIC="/home/cryptoforge/Qt/static/5.14.2"
 export MXE_PPATH="/home/cryptoforge/mxe/usr/bin"
-export APP_VERSION="1.0.6"
-export PREV_VERSION="1.0.5"
+export APP_VERSION="1.0.7"
+export PREV_VERSION="1.0.6"
 
 ./src/scripts/mkrelease.sh
 #./src/scripts/signbinaries.sh
